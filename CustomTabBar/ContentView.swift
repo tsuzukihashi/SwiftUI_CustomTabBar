a70f8a7e8bec7fe96fce7f6157cf226bf984ba9c
tsuzukihashi <tsudukihashi0817@gmail.com>
2019-11-26 21:51:54 +0900

    Initial Commit

---
+++ CustomTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTabBar
+//
+//  Created by Ryo Tsuzukihashi on 2019/11/26.
+//  Copyright © 2019 Ryo Tsuzukihashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
